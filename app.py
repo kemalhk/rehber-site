a3bc9a709ca12eb978a12c5da39bc334eacd5507
kemal <kemalharunkara@gmail.com>
2023-04-10 21:22:08 +0300

    veritabani models klasörünün altında oluşturuldu

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from .models.user import User,db,Rehber
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/myuser/rehber-site/instance/users.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'secret-key'
-db = SQLAlchemy(app)
+db.init_app(app)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(80), nullable=False)
+
 
 @app.route('/')
 def index():
