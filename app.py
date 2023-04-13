7442b908cd5ec2b7bf469fb41ad696d4aaeaf9c3
kemal <kemalharunkara@gmail.com>
2023-04-13 09:19:50 +0300

    git gunccelemen onceki hali

--- app.py
+++ app.py
@@ -1,114 +1,117 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from .models.user import User,db,Rehber
-import sys
+from .models.user import User, db, Rehber
+
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'secret-key'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = "secret-key"
 db.init_app(app)
 
 
-
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('login.html')
+    return render_template("login.html")
 
-@app.route('/register', methods=['GET', 'POST'])
+
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        #tekrarlanan kayıt engelleme
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        # tekrarlanan kayıt engelleme
         user = User.query.filter_by(username=username).first()
-        if user is  None:
+        if user is None:
             kayit = User(username=username, password=password)
             db.session.add(kayit)
             db.session.commit()
-            return redirect(url_for('login'))
+            return redirect(url_for("login"))
         else:
-            error = 'Kullanıcı adı kayıtlı'
-            return render_template('register.html', error=error)
-    return render_template('register.html')
+            error = "Kullanıcı adı kayıtlı"
+            return render_template("register.html", error=error)
+    return render_template("register.html")
 
-@app.route('/login', methods=['GET', 'POST'])
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
         user = User.query.filter_by(username=username, password=password).first()
         if user is not None:
-            return redirect(url_for('rehber'))
+            return redirect(url_for("rehber"))
         else:
-            error = 'Kullanıcı adı veya şifre hatalı.'
-            return render_template('login.html', error=error)
-    return render_template('login.html')
+            error = "Kullanıcı adı veya şifre hatalı."
+            return render_template("login.html", error=error)
+    return render_template("login.html")
 
 
-#giriş yaptıkdan sonraki sayfa
-@app.route('/rehber', methods=['GET', 'POST'])
+# giriş yaptıkdan sonraki sayfa
+@app.route("/rehber", methods=["GET", "POST"])
 def rehber():
-    return render_template('rehber.html')
+    return render_template("rehber.html")
 
 
-@app.route('/addNumber', methods=['GET', 'POST'])
+@app.route("/addNumber", methods=["GET", "POST"])
 def addNumber():
-    if request.method == 'POST':
-        ad = request.form['ad']
-        soyad = request.form['soyad']
-        telefon = request.form['telefon']
+    if request.method == "POST":
+        ad = request.form["ad"]
+        soyad = request.form["soyad"]
+        telefon = request.form["telefon"]
         kayit = Rehber(ad=ad, soyad=soyad, numara=telefon)
         db.session.add(kayit)
         db.session.commit()
-        mesaj='Kayıt başarıyla eklendi.'
-        return render_template('addNumber.html', mesaj=mesaj)
-    return render_template('addNumber.html')
+        mesaj = "Kayıt başarıyla eklendi."
+        return render_template("addNumber.html", mesaj=mesaj)
+    return render_template("addNumber.html")
+
 
 # Kayıt silme sayfası
-@app.route('/deleteNumber', methods=['GET', 'POST'])
+@app.route("/deleteNumber", methods=["GET", "POST"])
 def deleteNumber():
-    if request.method == 'POST':
-        id = request.form['id']
+    if request.method == "POST":
+        id = request.form["id"]
         user = Rehber.query.get(id)
         db.session.delete(user)
         db.session.commit()
-        return render_template('deleteNumber.html', mesaj='Kayıt başarıyla silindi.')
-    return render_template('deleteNumber.html')
+        return render_template("deleteNumber.html", mesaj="Kayıt başarıyla silindi.")
+    return render_template("deleteNumber.html")
+
 
 # Kayıt listeleme sayfası
-@app.route('/list', methods=['GET', 'POST'])
+@app.route("/list", methods=["GET", "POST"])
 def list():
     users = Rehber.query.all()
-    return render_template('list.html', users=users)
+    return render_template("list.html", users=users)
+
 
 # Kayıt güncelleme sayfası
-@app.route('/update', methods=['GET', 'POST'])
+@app.route("/update", methods=["GET", "POST"])
 def update():
-    
-    if request.method == 'POST':
-        id=request.form['id']
-        
-        #guncelle = Rehber.query.filter_by(Rehber.id==id).first()
-        #guncelle = Rehber.query.filter_by(id==id).first()
+    if request.method == "POST":
+        id = request.form["id"]
+
+        # guncelle = Rehber.query.filter_by(Rehber.id==id).first()
+        # guncelle = Rehber.query.filter_by(id==id).first()
 
         # Güncellenen kaydı veritabanından al
         guncelle = Rehber.query.get(id)
-        if guncelle:  
-            guncelle.ad=request.form['ad']
-            guncelle.soyad=request.form['soyad']
-            guncelle.numara=request.form['telefon']
+        if guncelle:
+            guncelle.ad = request.form["ad"]
+            guncelle.soyad = request.form["soyad"]
+            guncelle.numara = request.form["telefon"]
             db.session.commit()
-            return render_template('update.html',mesaj='kayıt başarıyla güncellendi')
+            return render_template("update.html", mesaj="kayıt başarıyla güncellendi")
         else:
-            return render_template('update.html', hata_mesaj='Kayıt bulunamadı')
-    return render_template('update.html')
-    
+            return render_template("update.html", hata_mesaj="Kayıt bulunamadı")
+    return render_template("update.html")
+
 
 # kod olmadan vt oluşmyor sorulucak
 with app.app_context():
     db.create_all()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
